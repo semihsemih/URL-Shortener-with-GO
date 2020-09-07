cad91734d78da8868e05f39d325318bb926ec45e
Semih Arslan <semihsemih@me.com>
2020-09-07 17:22:57 +0300

    Shorttened urls and real urls are stored in a map. 'mapHandler' function was created to check urls in a map. If there is a link, it redirects to the url.

--- main.go
+++ main.go
@@ -5,10 +5,20 @@ import (
 	"net/http"
 )
 
+
+
 func main() {
 	mux := defaultMux()
 
-	http.ListenAndServe(":9000", mux)
+	pathToUrls := map[string]string{
+		"/godoc": "https://godoc.org",
+		"/go-terminal-ui": "https://github.com/semihsemih/go-terminal-ui",
+	}
+
+	mapHandler := mapHandler(pathToUrls, mux)
+
+	fmt.Println("Server start on :8080")
+	http.ListenAndServe(":9000", mapHandler)
 }
 
 func defaultMux() *http.ServeMux  {
@@ -19,4 +29,15 @@ func defaultMux() *http.ServeMux  {
 
 func sayHello(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Hello, World")
+}
+
+func mapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc  {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		path := request.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(writer, request, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(writer, request)
+	}
 }
